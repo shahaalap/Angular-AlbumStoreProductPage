733fc0cd8f7f7e4a7e5e489890bc825d92b52086
shahaalap <68823276+shahaalap@users.noreply.github.com>
2021-02-08 07:42:30 -0500

    part 3 error

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,8 +11,6 @@ export class ProductService {
 
   getAlbum(id: number)
   {
-      return this._http.get(this._albumUrl).map((response) => {
-        return response.json();
-      });
+       return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
